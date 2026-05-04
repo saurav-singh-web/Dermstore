df68b35881cfb3372d10e8691128c2db769f7113
Saurav Singh <sauravsingh98866@gmail.com>
2026-05-05 02:56:05 +0530

    fix Express 5 wildcard CORS issue

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
-app.options('*', cors());
+
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
